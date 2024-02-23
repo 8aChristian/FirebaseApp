936414cc7597e3cef277710b150b643297dd9535
8aChristian <christian88afu@gmail.com>
2024-02-23 11:15:25 -0500

    FireBaseEsp32

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyATV49CFVIox8wYTOyB64VXvIX83I6lc3M",
+    authDomain: "insumo-3-firebase.firebaseapp.com",
+    projectId: "insumo-3-firebase",
+    storageBucket: "insumo-3-firebase.appspot.com",
+    messagingSenderId: "391168511096",
+    appId: "1:391168511096:web:fa03b2295ed2911a92318d"
+  }
 };
